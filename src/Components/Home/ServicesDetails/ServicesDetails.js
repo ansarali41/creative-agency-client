0e95873bfb56530b2a319be4f3a16b464fc22b9b
ansarali41 <ansar.ict14@gmail.com>
2020-10-16 18:56:57 +0600

    hover animation added

--- src/Components/Home/ServicesDetails/ServicesDetails.js
+++ src/Components/Home/ServicesDetails/ServicesDetails.js
@@ -6,7 +6,7 @@ const ServicesDetails = ({service}) => {
     const {title, description }=service;
     
     return (
-        <div className="col-md-4 text-center">
+        <div className="col-md-4 text-center zoom-out">
             <Link to='/dashboard' className="service-link">
                 <div className="shadow p-5 mb-5 bg-white rounded">
                     <img className="w-50 p-2" src={`data:image/png;base64,${service.image.img}`} alt="" />
